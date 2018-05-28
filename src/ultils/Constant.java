ef7d0bc749633eac4a2cd2185fa6d88dd0b49248
NguyenThanhTuan1234 <tuankunbeo@gmail.com>
2018-05-28 14:09:41 +0700

    Sua duong dan file

--- src/ultils/Constant.java
+++ src/ultils/Constant.java
@@ -4,8 +4,8 @@ public class Constant {
 	public static final String DATABASE_PROPERTIES_FILE = "database.properties";
 	public static final int NUMBER_ITEM = 8;
 	public static final int NUMBER_PRODUCT_PER_PAGE = 8;
-	public static final String PRODUCT_INPUT_FILE = "/home/tuankun/workspace/shopper/product_inputx";
-	public static final String SUB_CATEGORY_INPUT_FILE = "/home/tuankun/workspace/shopper/sub_category_inputx";
+	public static final String PRODUCT_INPUT_FILE = "product_inputx";
+	public static final String SUB_CATEGORY_INPUT_FILE = "sub_category_inputx";
 	public static final String CATEGORY_EXPORT_FILE = "category_export";
 	public static final String SUB_CATEGORY_EXPORT_FILE = "sub_category_export";
 	public static final String PRODUCT_EXPORT_FILE = "product_export";
